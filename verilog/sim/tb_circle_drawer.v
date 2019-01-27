290158fc1e8cf439d8bef98a4d7ccbd7edef493a
Patrick <hansenp2@tcnj.edu>
2019-01-27 15:41:31 -0500

    fixes to circle drawer for integration into circle drawing engine

--- verilog/sim/tb_circle_drawer.v
+++ verilog/sim/tb_circle_drawer.v
@@ -10,6 +10,15 @@ module tb_circle_drawer;
     reg [9:0] x0_in;
     reg [9:0] y0_in;
     reg [9:0] r_in;
+    reg [11:0] color;
+    
+    // input interface
+    reg  in_rts;
+    wire in_rtr;
+    
+    // output interface
+    wire  out_rts;                       
+    reg   out_rtr;
     
     // Unit Under Test
     circle_drawer uut(
@@ -17,7 +26,12 @@ module tb_circle_drawer;
         .rst_(rst_),  
         .x0_in(x0_in), 
         .y0_in(y0_in),
-        .r_in(r_in) 
+        .r_in(r_in),        
+        .color(color),
+        .in_rts(in_rts),
+        .in_rtr(in_rtr),
+        .out_rts(out_rts),
+        .out_rtr(out_rtr) 
     );
     
     initial 
@@ -31,11 +45,21 @@ module tb_circle_drawer;
         x0_in = 100;
         y0_in = 100;
         r_in  = 10;
+        color = 12'habc;
+        
+        out_rtr = 1;
         
         #10;
         
         #5 rst_ = ~rst_; clk = ~clk;
         #5 rst_ = ~rst_; clk = ~clk;
+        
+        #5; 
+            in_rts  = 1;
+            clk = ~clk;
+        #5; 
+            in_rts  = 0;
+            clk = ~clk;
         
         repeat(200)
         begin
--- verilog/src/circle_drawer.v
+++ verilog/src/circle_drawer.v
@@ -1,23 +1,52 @@
 `timescale 1ns / 1ps
 
 module circle_drawer(
+
+    // clock and sync reset
     input clk,
     input rst_,
     
+    // circle values
     input [9:0] x0_in,
     input [9:0] y0_in,
-    input [9:0] r_in
+    input [9:0] r_in,
+    input [11:0] color,
+    
+    // input interface
+    input  in_rts,
+    output in_rtr, 
+    
+    // output interface
+    output  out_rts,                       
+    input   out_rtr,
+    
+    output [9:0] draw_x_0, draw_x_1, draw_x_2, draw_x_3,
+    output [9:0] draw_x_4, draw_x_5, draw_x_6, draw_x_7,
+    
+    output [9:0] draw_y_0, draw_y_1, draw_y_2, draw_y_3,
+    output [9:0] draw_y_4, draw_y_5, draw_y_6, draw_y_7
 );
 
     // Circle Drawing Variables
-    wire [9:0] draw_x [0:7];
-    wire [9:0] draw_y [0:7]; 
+    // wire [9:0] draw_x [0:7];
+    // wire [9:0] draw_y [0:7]; 
     reg [9:0] x, y; 
     
     // Circle Drawing Components 
     reg [1:0] state;
     reg signed [9:0] dx, dy;
     reg signed [9:0] error; 
+    
+    // transfer completes
+    wire in_xfc, out_xfc;
+    assign in_xfc  = in_rts  & in_rtr;
+    assign out_xfc = out_rts & out_rtr;
+    
+    // ready to recieve when in state 00
+    assign in_rtr  = (state == 2'b00) ? (1) : (0);
+    
+    // ready to send when in state 01 or 10
+    assign out_rts = (state == 2'b01) ? (1) : (0);
     
     // State Machine
     always @ (posedge clk or negedge rst_)
@@ -36,16 +65,16 @@ module circle_drawer(
         begin
         
             // start state
-            if (state == 2'b00)
+            if (state == 2'b00 && in_xfc == 1)
                 state <= 2'b01;
                 
             // draw state
-            else if (state == 2'b01)
+            else if (state == 2'b01 && out_xfc == 1)
                 state <= (x >= y) ? (2'b01) : (2'b11); 
                
             // end state
             else if (state == 2'b11)
-                state <= 2'b11;
+                state <= 2'b00;
         end
     end
     
@@ -53,7 +82,7 @@ module circle_drawer(
     always @ (posedge clk or negedge rst_)
     begin
         
-        if (state == 2'b00)
+        if (state == 2'b00 && in_xfc == 1)
         begin
             x     <= r_in;
             y     <= 0;
@@ -62,7 +91,7 @@ module circle_drawer(
             error <= 0;
         end
         
-        else if (state == 2'b01)
+        else if (state == 2'b01 && out_xfc == 1)
         begin
             // write pixel data
             $display("*%d\t%d\t%d\t%d\t%d", x, y, $signed(dx), $signed(dy), $signed(error));
@@ -84,13 +113,23 @@ module circle_drawer(
         
     end
     
-    assign draw_x[0] = x0_in + x; assign draw_y[0] = y0_in + y;
+    /* assign draw_x[0] = x0_in + x; assign draw_y[0] = y0_in + y;
     assign draw_x[1] = x0_in + y; assign draw_y[1] = y0_in + x;    
     assign draw_x[2] = x0_in - y; assign draw_y[2] = y0_in + x;    
     assign draw_x[3] = x0_in - x; assign draw_y[3] = y0_in + y;    
     assign draw_x[4] = x0_in - x; assign draw_y[4] = y0_in - y;    
     assign draw_x[5] = x0_in - y; assign draw_y[5] = y0_in - x;    
     assign draw_x[6] = x0_in + y; assign draw_y[6] = y0_in - x;    
-    assign draw_x[7] = x0_in + x; assign draw_y[7] = y0_in - y;
+    assign draw_x[7] = x0_in + x; assign draw_y[7] = y0_in - y; */
+    
+    assign draw_x_0 = x0_in + x; assign draw_y_0 = y0_in + y;
+    assign draw_x_1 = x0_in + y; assign draw_y_1 = y0_in + x;    
+    assign draw_x_2 = x0_in - y; assign draw_y_2 = y0_in + x;    
+    assign draw_x_3 = x0_in - x; assign draw_y_3 = y0_in + y;    
+    assign draw_x_4 = x0_in - x; assign draw_y_4 = y0_in - y;    
+    assign draw_x_5 = x0_in - y; assign draw_y_5 = y0_in - x;    
+    assign draw_x_6 = x0_in + y; assign draw_y_6 = y0_in - x;    
+    assign draw_x_7 = x0_in + x; assign draw_y_7 = y0_in - y;
         
+
 endmodule
