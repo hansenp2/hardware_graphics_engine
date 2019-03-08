863f6b7ab9896b4f8a68dd4cff532515c1788ecc
Patrick <hansenp2@tcnj.edu>
2019-03-08 11:34:29 -0500

    circle drawing fixes and integration

--- verilog/src/circle_drawer.v
+++ verilog/src/circle_drawer.v
@@ -1,42 +1,44 @@
 `timescale 1ns / 1ps
 
-`define INIT_STATE  0
-`define START_STATE 1
-`define DRAW_STATE  2
-`define END_STATE   3
+`define START_STATE         0
+`define INIT_STATE          1
+`define PRE_DRAW_STATE      2
+`define DRAW_STATE          3
+`define DRAW_SETUP_STATE    4
+`define END_STATE           5
 
 module circle_drawer(
 
-    // Clock and sync reset
+    // clock and sync reset
     input clk,
     input rst_,
 
-    // Circle values
+    // circle values
     input [9:0] x0_in,
     input [9:0] y0_in,
     input [9:0] r_in,
     input [11:0] color,
 
-    // Input interface
+    // input interface
     input  in_rts,
     output in_rtr,
 
-    // Output interface
+    // output interface
     output  out_rts,
     input   out_rtr,
 
-    output [9:0] draw_x_0, draw_x_1, draw_x_2, draw_x_3,
-    output [9:0] draw_x_4, draw_x_5, draw_x_6, draw_x_7,
+    output reg [9:0] draw_x_0, draw_x_1, draw_x_2, draw_x_3,
+    output reg [9:0] draw_x_4, draw_x_5, draw_x_6, draw_x_7,
 
-    output [9:0] draw_y_0, draw_y_1, draw_y_2, draw_y_3,
-    output [9:0] draw_y_4, draw_y_5, draw_y_6, draw_y_7
+    output reg [9:0] draw_y_0, draw_y_1, draw_y_2, draw_y_3,
+    output reg [9:0] draw_y_4, draw_y_5, draw_y_6, draw_y_7
 );
 
-    // Circle drawing variables
+    // Circle Drawing Variables
     reg [9:0] x, y;
 
-    // Circle drawing components
-    reg [1:0] state;
+    // Circle Drawing Components
+    reg [2:0] state;
     reg signed [9:0] dx, dy;
     reg signed [9:0] error;
 
@@ -45,10 +47,10 @@ module circle_drawer(
     assign in_xfc  = in_rts  & in_rtr;
     assign out_xfc = out_rts & out_rtr;
 
-    // Ready to recieve when in state START STATE
+    // Ready to recieve when in state 00
     assign in_rtr  = (state == `START_STATE) ? (1) : (0);
 
-    // Ready to send when in state DRAW STATE
+    // Ready to send when in state 01 or 10
     assign out_rts = (state == `DRAW_STATE) ? (1) : (0);
 
     // State machine
@@ -59,17 +61,23 @@ module circle_drawer(
 
         else
         begin
+
             // Start state
             if (state == `START_STATE && in_xfc == 1)
                 state <= `INIT_STATE;
 
             // Init state
             if (state == `INIT_STATE)
-                state <= `DRAW_STATE;
+                state <= `PRE_DRAW_STATE;
 
-            // Draw state
-            else if (state == `DRAW_STATE && out_xfc == 1)
+            if (state == `PRE_DRAW_STATE)
                 state <= (x >= y) ? (`DRAW_STATE) : (`END_STATE);
+
+            if (state == `DRAW_STATE && out_xfc == 1)
+                state <= `DRAW_SETUP_STATE;
+
+            if (state == `DRAW_SETUP_STATE)
+                state <= `PRE_DRAW_STATE;
 
             // End state
             else if (state == `END_STATE)
@@ -92,19 +100,33 @@ module circle_drawer(
 
         else
         begin
-            if (state == `START_STATE && in_xfc == 1)
+            if (state == `INIT_STATE)
             begin
                 x  <= r_in;
                 y  <= 0;
                 dx <= 1 - (r_in << 1);
                 dy <= 1;
                 error <= 0;
+                $display("*******************");
             end
 
-            else if (state == `DRAW_STATE && out_xfc == 1)
+            //else if (state == `DRAW_STATE)
+            else if (state == `PRE_DRAW_STATE)
+            begin
+                draw_x_0 <= x0_in + x; draw_y_0 <= y0_in + y;
+                draw_x_1 <= x0_in + y; draw_y_1 <= y0_in + x;
+                draw_x_2 <= x0_in - y; draw_y_2 <= y0_in + x;
+                draw_x_3 <= x0_in - x; draw_y_3 <= y0_in + y;
+                draw_x_4 <= x0_in - x; draw_y_4 <= y0_in - y;
+                draw_x_5 <= x0_in - y; draw_y_5 <= y0_in - x;
+                draw_x_6 <= x0_in + y; draw_y_6 <= y0_in - x;
+                draw_x_7 <= x0_in + x; draw_y_7 <= y0_in - y;
+            end
+
+            else if (state == `DRAW_SETUP_STATE)
             begin
                 // Write pixel data
-                $display("*%d\t%d\t%d\t%d\t%d", x, y, $signed(dx), $signed(dy), $signed(error));
+                // $display("*%d\t%d\t%d\t%d\t%d", x, y, $signed(dx), $signed(dy), $signed(error));
 
                 if ( (((error + dy) << 1) + dx) > 0 )
                 begin
@@ -120,16 +142,19 @@ module circle_drawer(
                     dy <= dy + 2;
                 end
             end
+
+            else if (state == `DRAW_STATE && out_xfc == 1)
+                $display("(%d,%d), (%d,%d), (%d,%d), (%d,%d), (%d,%d), (%d,%d), (%d,%d), (%d,%d)",
+                    x0_in + x, y0_in + y,
+                    x0_in + y, y0_in + x,
+                    x0_in - y, y0_in + x,
+                    x0_in - x, y0_in + y,
+                    x0_in - x, y0_in - y,
+                    x0_in - y, y0_in - x,
+                    x0_in + y, y0_in - x,
+                    x0_in + x, y0_in - y);
+
         end
     end
-
-    assign draw_x_0 = x0_in + x; assign draw_y_0 = y0_in + y;
-    assign draw_x_1 = x0_in + y; assign draw_y_1 = y0_in + x;
-    assign draw_x_2 = x0_in - y; assign draw_y_2 = y0_in + x;
-    assign draw_x_3 = x0_in - x; assign draw_y_3 = y0_in + y;
-    assign draw_x_4 = x0_in - x; assign draw_y_4 = y0_in - y;
-    assign draw_x_5 = x0_in - y; assign draw_y_5 = y0_in - x;
-    assign draw_x_6 = x0_in + y; assign draw_y_6 = y0_in - x;
-    assign draw_x_7 = x0_in + x; assign draw_y_7 = y0_in - y;
 
 endmodule
