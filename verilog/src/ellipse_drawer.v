68f96c029df4227fa28211a31839033b13691186
Patrick <hansenp2@tcnj.edu>
2019-03-15 11:43:42 -0400

    ellipse drawing engine completed

--- verilog/src/ellipse_drawer.v
+++ verilog/src/ellipse_drawer.v
@@ -1,125 +1,229 @@
 `timescale 1ns / 1ps
 
+`define E_START_STATE           0
+`define E_INIT_STATE            1
+`define E_PRE_DRAW_STATE_1      2
+`define E_PRE_DRAW_STATE_2      3
+`define E_DRAW_STATE            4
+`define E_POST_DRAW_STATE       5
+`define E_DRAW_SETUP_STATE_1    6
+`define E_DRAW_SETUP_STATE_2    7
+`define E_END_STATE             8
+
 module ellipse_drawer(
     input clk,
     input rst_,
-    
+
     input [9:0] x0_in, y0_in,
-    input [9:0] a_in, b_in
+    input [9:0] a_in, b_in,
+    input [11:0] color,
+
+    // input interface
+    input  in_rts,
+    output in_rtr,
+
+    // output interface
+    output  out_rts,
+    input   out_rtr,
+
+    output reg [9:0] draw_x_0, draw_x_1, draw_x_2, draw_x_3,
+    output reg [9:0] draw_y_0, draw_y_1, draw_y_2, draw_y_3,
+    output reg [11:0] color_hold
 );
 
-    // Ellipse Variables
+    reg [9:0] x0_hold, y0_hold;
+    reg [9:0] a_hold, b_hold;
+
+    // Ellipse variables
     reg [9:0] x, y;
-    reg signed [15:0] d1, d2;
-    
-    // Temporary Variables
-    wire signed [15:0] t1, t2, t3;
-    wire signed [15:0] t4, t5, t6, t7;
-    reg  signed [15:0] t8, t9;
-    reg [1:0] state;
-    
-    // State Machine
+    reg signed [31:0] d1, d2;
+
+    // Temporary variables
+    wire signed [31:0] t1, t2, t3;
+    wire signed [31:0] t4, t5, t6, t7;
+    reg  signed [31:0] t8, t9;
+    reg [2:0] state;
+
+    // Transfer completes
+    wire in_xfc, out_xfc;
+    assign in_xfc  = in_rts  & in_rtr;
+    assign out_xfc = out_rts & out_rtr;
+
+    // Ready to recieve when in start state
+    assign in_rtr  = (state == `E_START_STATE) ? 1 : 0;
+
+    // Ready to send when in draw state
+    assign out_rts = (state == `E_DRAW_STATE) ? 1 : 0;
+
+    // State machine
     always @ (posedge clk or negedge rst_)
     begin
         if (!rst_)
-        begin
-            state <= 2'b00;
-            x     <= 0;
-            y     <= 0;
-            d1    <= 0;
-            d2    <= 0;
-            t8    <= 0;
-            t9    <= 0;
-        end
-            
+            state <= `E_START_STATE;
+
         else
         begin
-            // start state
-            if (state == 2'b00)
-                state <= 2'b01;
-                
-            // draw state
-            else if (state == 2'b01)
-                state <= (d2 < 0) ? (2'b01) : (2'b10); 
-                
-            // draw state
-            else if (state == 2'b10)
-                state <= (x != 0) ? (2'b10) : (2'b11); 
+            case (state)
+                `E_START_STATE:
+                begin
+                    if (in_xfc)
+                        state <= `E_INIT_STATE;
+                end
 
-            // end state
-            else if (state == 2'b11)
-                state <= 2'b11;
-        end 
+                `E_INIT_STATE:
+                    state <= `E_PRE_DRAW_STATE_1;
+
+                `E_PRE_DRAW_STATE_1:
+                    state <= `E_DRAW_STATE;
+
+                `E_PRE_DRAW_STATE_2:
+                begin
+                    if (x != ((2 << 9) - 1))
+                        state <= `E_DRAW_STATE;
+                    else
+                        state <= `E_END_STATE;
+                end
+
+                `E_DRAW_STATE:
+                begin
+                    if (out_xfc)
+                        state <= `E_POST_DRAW_STATE;
+                end
+
+                `E_POST_DRAW_STATE:
+                begin
+                    if (d2 < 0)
+                        state <= `E_DRAW_SETUP_STATE_1;
+                    else
+                        state <= `E_DRAW_SETUP_STATE_2;
+                end
+
+                `E_DRAW_SETUP_STATE_1:
+                    state <= `E_PRE_DRAW_STATE_1;
+
+                `E_DRAW_SETUP_STATE_2:
+                    state <= `E_PRE_DRAW_STATE_2;
+
+                `E_END_STATE:
+                    state <= `E_START_STATE;
+
+                default:
+                    state <= 0;
+            endcase
+        end
     end
-    
-    // Drawing Engine  
-    assign t1 = a_in ** 2;
+
+    // Drawing engine
+    assign t1 = a_hold ** 2;
     assign t2 = t1 << 1;
     assign t3 = t2 << 1;
-    assign t4 = b_in ** 2;
+    assign t4 = b_hold ** 2;
     assign t5 = t4 << 1;
     assign t6 = t5 << 1;
-    assign t7 = a_in * t5;    
-    
+    assign t7 = a_hold * t5;
+
     always @ (posedge clk or negedge rst_)
     begin
-        
-        if (state == 2'b00)
+
+        if (!rst_)
         begin
-            x  <= a_in;
-            y  <= 0;
-            
-            t8 <= t7 << 1; 
+            x <= 0;
+            y <= 0;
+            d1 <= 0;
+            d2 <= 0;
+            t8 <= 0;
             t9 <= 0;
-            
-            d1 <= (t2 - t7 + t4) >>> 1;
-            d2 <= (t1 >> 1) - (t7 << 1) + t5;
+            x0_hold <= 0;
+            y0_hold <= 0;
+            a_hold <= 0;
+            b_hold <= 0;
+            color_hold <= 0;
         end
-        
-        else if (state == 2'b01 && d2 < 0)
+
+        else
         begin
-            // draw pixels here
-            $display("*%d\t%d\t%d\t%d\t%d\t%d", x, y, $signed(d1), $signed(d2), $signed(t8), $signed(t9));
-            
-            y  <= y + 1;                       
-            t9 <= t9 + t3; 
-            
-            if (d1 < 0)  
-            begin                    
-                d1 <= d1 + (t9 + t3) + t2;
-                d2 <= d2 + (t9 + t3);
-            end 
-            
-            else
-            begin                           
-                x  <= x - 1;
-                t8 <= t8 - t6; 
-                d1 <= d1 - (t8 - t6) + (t9 + t3) + t2; 
-                d2 <= d2 - (t8 - t6) + t5 + (t9 + t3);
-            end
-            
-        end 
-        
-        else if (state == 2'b10)
-        begin
-            //draw pixels here
-            $display("**%d\t%d\t%d\t%d\t%d\t%d", x, y, $signed(d1), $signed(d2), $signed(t8), $signed(t9));
-            
-            x  <= x - 1;                       
-            t8 <= t8 - t6; 
-            
-            if (d2 < 0)
-            begin       
-                y  <= y + 1;
-                t9 <= t9 + t3; 
-                d2 <= d2 - (t8 - t6) + t5 + (t9 + t3); 
-            end
-            
-            else                         
-                d2 <= d2 - (t8 - t6) + t5; 
-        
-        end 
-        
+            case (state)
+                `E_START_STATE:
+                begin
+                    if (in_xfc)
+                    begin
+                        x0_hold <= x0_in;
+                        y0_hold <= y0_in;
+                        a_hold <= a_in;
+                        b_hold <= b_in;
+                        color_hold <= color;
+                        x <= 0;
+                        y <= 0;
+                    end
+                end
+
+                `E_INIT_STATE:
+                begin
+                    x <= a_hold;
+                    y <= 0;
+                    t8 <= t7 << 1;
+                    t9 <= 0;
+                    d1 <= (t2 - t7 + t4) >>> 1;
+                    d2 <= (t1 >>> 1) - (t7 << 1) + t5;
+                end
+
+                `E_PRE_DRAW_STATE_1:
+                begin
+                    draw_x_0 <= x0_hold + x; draw_y_0 <= y0_hold + y;
+                    draw_x_1 <= x0_hold + x; draw_y_1 <= y0_hold - y;
+                    draw_x_2 <= x0_hold - x; draw_y_2 <= y0_hold + y;
+                    draw_x_3 <= x0_hold - x; draw_y_3 <= y0_hold - y;
+                end
+
+                `E_PRE_DRAW_STATE_2:
+                begin
+                    draw_x_0 <= x0_hold + x; draw_y_0 <= y0_hold + y;
+                    draw_x_1 <= x0_hold + x; draw_y_1 <= y0_hold - y;
+                    draw_x_2 <= x0_hold - x; draw_y_2 <= y0_hold + y;
+                    draw_x_3 <= x0_hold - x; draw_y_3 <= y0_hold - y;
+                end
+
+                `E_DRAW_SETUP_STATE_1:
+                begin
+                    if (d2 < 0)
+                        begin
+                        y <= y + 1;
+                        t9 <= t9 + t3;
+
+                        if (d1 < 0)
+                        begin
+                            d1 <= d1 + (t9 + t3) + t2;
+                            d2 <= d2 + (t9 + t3);
+                        end
+
+                        else
+                        begin
+                            x <= x - 1;
+                            t8 <= t8 - t6;
+                            d1 <= d1 - (t8 - t6) + (t9 + t3) + t2;
+                            d2 <= d2 - (t8 - t6) + t5 + (t9 + t3);
+                        end
+                    end
+                end
+
+                `E_DRAW_SETUP_STATE_2:
+                begin
+                    x  <= x - 1;
+                    t8 <= t8 - t6;
+
+                    if (d2 < 0)
+                    begin
+                        y  <= y + 1;
+                        t9 <= t9 + t3;
+                        d2 <= d2 - (t8 - t6) + t5 + (t9 + t3);
+                    end
+
+                    else
+                        d2 <= d2 - (t8 - t6) + t5;
+                end
+            endcase
+        end
+
     end
-    
+
 endmodule
---
+++ verilog/src/ellipse_drawing_engine.v
@@ -0,0 +1,147 @@
+`timescale 1ns / 1ps
+
+// INPUT FIFO PARAMETERS
+`define E_IN_FIFO_DATA_WIDTH  52
+`define E_IN_FIFO_DEPTH        4
+`define E_IN_FIFO_LOG2DEPTH    2
+
+// OUTPUT FIFO PARAMETERS
+`define E_OUT_FIFO_DATA_WIDTH  10
+`define E_OUT_FIFO_DEPTH      128
+`define E_OUT_FIFO_LOG2DEPTH    7
+
+module ellipse_drawing_engine(
+
+    // clock and sync reset
+    input clk,
+    input rst_,
+
+    // input interface
+    input [51:0] in_op,
+    input  in_rts,
+    output in_rtr,
+
+    // output interface
+    output out_rts,
+    input  out_rtr,
+
+    // arbiter interface
+    input bcast_xfc,
+    input [31:0] arb_data_in,
+    output [31:0] arb_data_out,
+    output [16:0] arb_addr_out,
+    output [3:0] wr_op
+);
+
+    // Input fifo for ops
+    wire [51:0] current_op;
+    wire fi_rts_ed, ed_rtr_fi;
+    fifo #(`E_IN_FIFO_DATA_WIDTH, `E_IN_FIFO_DEPTH, `E_IN_FIFO_LOG2DEPTH) fi (
+        .clk(clk),
+        .rst_(rst_),
+
+        .in_data(in_op),
+        .in_rts(in_rts),
+        .in_rtr(in_rtr),
+
+        .out_data(current_op),
+        .out_rts(fi_rts_ed),
+        .out_rtr(ed_rtr_fi)
+    );
+
+
+    // Line drawing algorithm module
+    wire ed_rts_fx, rx_rtr_ed;
+    wire [9:0] draw_x [0:3];
+    wire [9:0] draw_y [0:3];
+    wire [11:0] color_out;
+    ellipse_drawer ed(
+        .clk(clk),
+        .rst_(rst_),
+        .x0_in(current_op[51:42]),
+        .y0_in(current_op[41:32]),
+        .a_in(current_op[31:22]),
+        .b_in(current_op[21:12]),
+        .color(current_op[11:0]),
+
+        .in_rts(fi_rts_ed),
+        .in_rtr(ed_rtr_fi),
+
+        .out_rts(ed_rts_fx),
+        .out_rtr(rx_rtr_ed),
+
+        .draw_x_0(draw_x[0]),
+        .draw_x_1(draw_x[1]),
+        .draw_x_2(draw_x[2]),
+        .draw_x_3(draw_x[3]),
+
+        .draw_y_0(draw_y[0]),
+        .draw_y_1(draw_y[1]),
+        .draw_y_2(draw_y[2]),
+        .draw_y_3(draw_y[3]),
+        .color_hold(color_out)
+    );
+
+    // Output fifos
+    wire [31:0] f_out;
+    wire fo_rts_ae, ae_rtr_fo;
+    ellipse_buffer_out #(32, `E_OUT_FIFO_DEPTH, `E_OUT_FIFO_LOG2DEPTH) fo (
+        .clk(clk),
+        .rst_(rst_),
+
+        .in_px_0({ draw_x[0], draw_y[0], color_out}),
+        .in_px_1({ draw_x[1], draw_y[1], color_out}),
+        .in_px_2({ draw_x[2], draw_y[2], color_out}),
+        .in_px_3({ draw_x[3], draw_y[3], color_out}),
+
+        .in_rts(ed_rts_fx),
+        .in_rtr(rx_rtr_ed),
+
+        .out_data(f_out),
+        .out_rts(fo_rts_ae),
+        .out_rtr(ae_rtr_fo)
+    );
+
+    // Addressing engine
+    wire [16:0] addr_base;
+    wire [ 2:0] addr_offset;
+    wire [11:0] color_rmw;
+    wire ae_rts_rmw, rmw_rtr_ae;
+
+    addressing_engine ae (
+        .clk(clk),
+        .rst_(rst_),
+        .cmd_data_origx(f_out[31:22]),
+        .cmd_data_origy(f_out[21:12]),
+        .in_color(f_out[11:0]),
+        .init_addr(addr_base),
+        .addr_offset(addr_offset),
+        .out_color(color_rmw),
+        .in_rts(fo_rts_ae),
+        .in_rtr(ae_rtr_fo),
+        .out_rts(ae_rts_rmw),
+        .out_rtr(rmw_rtr_ae)
+    );
+
+
+    // Read-Modify-Write engine
+    read_modify_write_engine rmw (
+        .clk(clk),
+        .rst_(rst_),
+        .addr_base(addr_base),
+        .addr_offset(addr_offset),
+        .color(color_rmw),
+
+        .addr_rts(ae_rts_rmw),
+        .addr_rtr(rmw_rtr_ae),
+
+        .in_data(arb_data_in),
+        .out_data(arb_data_out),
+        .out_addr(arb_addr_out),
+        .arb_rts(out_rts),
+        .arb_rtr(out_rtr),
+        .bcast_xfc(bcast_xfc),
+        .wr_op(wr_op)
+    );
+
+endmodule
