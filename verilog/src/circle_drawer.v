fe3c9f099db569b99df5b1a68da84bc965a04c93
Patrick <hansenp2@tcnj.edu>
2019-04-22 17:06:18 -0400

    fixed circle drawer state machine

--- verilog/src/circle_drawer.v
+++ verilog/src/circle_drawer.v
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 
-`define START_STATE         0
-`define INIT_STATE          1
-`define PRE_DRAW_STATE      2
-`define DRAW_STATE          3
-`define DRAW_SETUP_STATE    4
-`define END_STATE           5
+`define C_START_STATE         0
+`define C_INIT_STATE          1
+`define C_PRE_DRAW_STATE      2
+`define C_DRAW_STATE          3
+`define C_DRAW_SETUP_STATE    4
+`define C_END_STATE           5
 
 module circle_drawer(
 
@@ -31,8 +31,13 @@ module circle_drawer(
     output reg [9:0] draw_x_4, draw_x_5, draw_x_6, draw_x_7,
 
     output reg [9:0] draw_y_0, draw_y_1, draw_y_2, draw_y_3,
-    output reg [9:0] draw_y_4, draw_y_5, draw_y_6, draw_y_7
+    output reg [9:0] draw_y_4, draw_y_5, draw_y_6, draw_y_7,
+    output reg [11:0] color_hold
 );
+
+    reg [9:0] x0_hold;
+    reg [9:0] y0_hold;
+    reg [9:0] r_hold;
 
     // Circle Drawing Variables
     reg [9:0] x, y;
@@ -47,41 +52,47 @@ module circle_drawer(
     assign in_xfc  = in_rts  & in_rtr;
     assign out_xfc = out_rts & out_rtr;
 
-    // Ready to recieve when in state 00
-    assign in_rtr  = (state == `START_STATE) ? (1) : (0);
+    // Ready to recieve when in start state
+    assign in_rtr  = (state == `C_START_STATE) ? (1) : (0);
 
-    // Ready to send when in state 01 or 10
-    assign out_rts = (state == `DRAW_STATE) ? (1) : (0);
+    // Ready to send when in draw state
+    assign out_rts = (state == `C_DRAW_STATE) ? (1) : (0);
 
     // State machine
     always @ (posedge clk or negedge rst_)
     begin
         if (!rst_)
-            state <= `START_STATE;
+            state <= `C_START_STATE;
 
         else
         begin
 
-            // Start state
-            if (state == `START_STATE && in_xfc == 1)
-                state <= `INIT_STATE;
+            case (state)
+                `C_START_STATE:
+                begin
+                    if (in_xfc)
+                        state <= `C_INIT_STATE;
+                end
 
-            // Init state
-            if (state == `INIT_STATE)
-                state <= `PRE_DRAW_STATE;
+                `C_INIT_STATE:
+                    state <= `C_PRE_DRAW_STATE;
 
-            if (state == `PRE_DRAW_STATE)
-                state <= (x >= y) ? (`DRAW_STATE) : (`END_STATE);
+                `C_PRE_DRAW_STATE:
+                    state <= (x >= y) ? `C_DRAW_STATE : `C_END_STATE;
 
-            if (state == `DRAW_STATE && out_xfc == 1)
-                state <= `DRAW_SETUP_STATE;
+                `C_DRAW_STATE:
+                begin
+                    if (out_xfc)
+                        state <= `C_DRAW_SETUP_STATE;
+                end
 
-            if (state == `DRAW_SETUP_STATE)
-                state <= `PRE_DRAW_STATE;
+                `C_DRAW_SETUP_STATE:
+                    state <= `C_PRE_DRAW_STATE;
 
-            // End state
-            else if (state == `END_STATE)
-                state <= `START_STATE;
+                `C_END_STATE:
+                    state <= `C_START_STATE;
+            endcase
+
         end
     end
 
@@ -96,63 +107,61 @@ module circle_drawer(
             dx <= 0;
             dy <= 0;
             error <= 0;
+            x0_hold <= 0;
+            y0_hold <= 0;
+            r_hold <= 0;
+            color_hold <= 0;
         end
 
         else
         begin
-            if (state == `INIT_STATE)
-            begin
-                x  <= r_in;
-                y  <= 0;
-                dx <= 1 - (r_in << 1);
-                dy <= 1;
-                error <= 0;
-                $display("*******************");
-            end
-
-            //else if (state == `DRAW_STATE)
-            else if (state == `PRE_DRAW_STATE)
-            begin
-                draw_x_0 <= x0_in + x; draw_y_0 <= y0_in + y;
-                draw_x_1 <= x0_in + y; draw_y_1 <= y0_in + x;
-                draw_x_2 <= x0_in - y; draw_y_2 <= y0_in + x;
-                draw_x_3 <= x0_in - x; draw_y_3 <= y0_in + y;
-                draw_x_4 <= x0_in - x; draw_y_4 <= y0_in - y;
-                draw_x_5 <= x0_in - y; draw_y_5 <= y0_in - x;
-                draw_x_6 <= x0_in + y; draw_y_6 <= y0_in - x;
-                draw_x_7 <= x0_in + x; draw_y_7 <= y0_in - y;
-            end
-
-            else if (state == `DRAW_SETUP_STATE)
-            begin
-                // Write pixel data
-                // $display("*%d\t%d\t%d\t%d\t%d", x, y, $signed(dx), $signed(dy), $signed(error));
-
-                if ( (((error + dy) << 1) + dx) > 0 )
+            case (state)
+                `C_START_STATE:
                 begin
-                    x <= x - 1;
-                    error <= error + dx;
-                    dx <= dx + 2;
+                    x0_hold <= x0_in;
+                    y0_hold <= y0_in;
+                    r_hold <= r_in;
+                    color_hold <= color;
                 end
 
-                else
+                `C_INIT_STATE:
                 begin
-                    y <= y + 1;
-                    error <= error + dy;
-                    dy <= dy + 2;
+                    x  <= r_hold;
+                    y  <= 0;
+                    dx <= 1 - (r_hold << 1);
+                    dy <= 1;
+                    error <= 0;
                 end
-            end
 
-            else if (state == `DRAW_STATE && out_xfc == 1)
-                $display("(%d,%d), (%d,%d), (%d,%d), (%d,%d), (%d,%d), (%d,%d), (%d,%d), (%d,%d)",
-                    x0_in + x, y0_in + y,
-                    x0_in + y, y0_in + x,
-                    x0_in - y, y0_in + x,
-                    x0_in - x, y0_in + y,
-                    x0_in - x, y0_in - y,
-                    x0_in - y, y0_in - x,
-                    x0_in + y, y0_in - x,
-                    x0_in + x, y0_in - y);
+                `C_PRE_DRAW_STATE:
+                begin
+                    draw_x_0 <= x0_hold + x; draw_y_0 <= y0_hold + y;
+                    draw_x_1 <= x0_hold + y; draw_y_1 <= y0_hold + x;
+                    draw_x_2 <= x0_hold - y; draw_y_2 <= y0_hold + x;
+                    draw_x_3 <= x0_hold - x; draw_y_3 <= y0_hold + y;
+                    draw_x_4 <= x0_hold - x; draw_y_4 <= y0_hold - y;
+                    draw_x_5 <= x0_hold - y; draw_y_5 <= y0_hold - x;
+                    draw_x_6 <= x0_hold + y; draw_y_6 <= y0_hold - x;
+                    draw_x_7 <= x0_hold + x; draw_y_7 <= y0_hold - y;
+                end
+
+                `C_DRAW_SETUP_STATE:
+                begin
+                    if ( (((error + dy) << 1) + dx) > 0 )
+                    begin
+                        x <= x - 1;
+                        error <= error + dx;
+                        dx <= dx + 2;
+                    end
+
+                    else
+                    begin
+                        y <= y + 1;
+                        error <= error + dy;
+                        dy <= dy + 2;
+                    end
+                end
+            endcase
 
         end
     end
